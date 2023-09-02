020edbccd22e576d6f8ef21246a7d289699f1a3c
Ana Montero <anamongar@gmail.com>
2023-09-03 00:18:56 +0200

    Tested exception is thrown if id is not found

--- src/main/java/montero/ana/monsterservice/controller/MonsterController.java
+++ src/main/java/montero/ana/monsterservice/controller/MonsterController.java
@@ -2,6 +2,8 @@ package montero.ana.monsterservice.controller;
 
 import montero.ana.monsterservice.domain.Monster;
 import montero.ana.monsterservice.service.MonsterService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,22 @@ import java.util.List;
 @RestController
 @RequestMapping("api/monsters")
 public class MonsterController {
-    final
-    MonsterService monsterService;
+    private final MonsterService monsterService;
 
-    public MonsterController(MonsterService monsterService) {
+    private final ServletWebServerApplicationContext webServerAppCtxt;
+
+    public MonsterController(MonsterService monsterService, ServletWebServerApplicationContext webServerAppCtxt) {
         this.monsterService = monsterService;
+        this.webServerAppCtxt = webServerAppCtxt;
     }
 
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @GetMapping("/who-am-i")
+    public String whoAmI(){
+        return "Hello I'm " + applicationName + " and I'm running on port: " + webServerAppCtxt.getWebServer().getPort();
+    }
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Monster> getAll(){
